c53a3d35649061f03ee74f693bfe6ba42128053a
Sergey Melkonyan <sergey.melkonyan2005@gmail.com>
2022-09-07 11:48:29 +0400

    Initial Commit

---
+++ Tweego/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tweego
+//
+//  Created by Sergey Melkonyan on 07.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
